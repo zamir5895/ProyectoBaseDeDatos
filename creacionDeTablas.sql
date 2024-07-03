a350db8fbbdf2d202acecd2c0c74511a43510903
zamir5895 <lmr293141@gmail.com>
2024-07-03 17:25:14 -0500

    Nuevo modelo entidad relacion

--- creacionDeTablas.sql
+++ creacionDeTablas.sql
@@ -1,170 +1,112 @@
-CREATE SCHEMA ConnectBaseDeDatos;
-SET search_path TO ConnectBaseDeDatos;
-
-CREATE TABLE IF NOT EXISTS Usuario(
-    ID VARCHAR(10) PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS Persona(
+    dni VARCHAR(8) PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     apellido VARCHAR(50) NOT NULL,
-    contrasenia VARCHAR(50) NOT NULL,
-    fecharegistro DATE NOT NULL,
-    fechaNacimiento DATE NOT NULL,
-    correo VARCHAR(50) NOT NULL,
-    direccion VARCHAR(50) NOT NULL
+    edad INTEGER NOT NULL,
+    direccion VARCHAR(50) NOT NULL,
+    telefono INTEGER NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    fechaNacimiento DATE NOT NULL
 );
+CREATE TABLE IF NOT EXISTS Empleado(
+    dni VARCHAR(8) PRIMARY KEY,
+    FOREIGN KEY (dni) REFERENCES Persona(dni),
+    cargo VARCHAR(50) NOT NULL,
+    sueldo DECIMAL NOT NULL
+);
+CREATE TABLE IF NOT EXISTS Medico(
+    dni VARCHAR(8) PRIMARY KEY,
+    FOREIGN KEY (dni) REFERENCES Persona(dni),
+    añoEgreso DATE NOT NULL
+);
+CREATE TABLE IF NOT EXISTS Paciente(
+    dni VARCHAR(8) PRIMARY KEY,
+    FOREIGN KEY (dni) REFERENCES Persona(dni),
+    nroSeguro VARCHAR(50) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS Especialidad(
+    id VARCHAR(10) PRIMARY KEY,
+    dni VARCHAR(8) NOT NULL,
+    FOREIGN KEY (dni) REFERENCES Medico(dni),
+    nombre VARCHAR(50) NOT NULL,
+    descripcion VARCHAR(50) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS Consultorio(
+    id VARCHAR(10) PRIMARY KEY,
+    capacidad INTEGER NOT NULL,
+    cantidadEmpleado INTEGER NOT NULL,
+    cantidadPaciente INTEGER NOT NULL,
+    direccion VARCHAR(50) NOT NULL,
+    telefono INTEGER NOT NULL
+);
+CREATE TABLE IF NOT EXISTS Enfermedad(
+    id VARCHAR(10) PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    descripcion VARCHAR(50) NOT NULL
 
-CREATE TABLE IF NOT EXISTS Viajero(
-    ID VARCHAR(10) PRIMARY KEY,
-    FOREIGN KEY (ID) REFERENCES Usuario(ID)
 );
-CREATE TABLE IF NOT EXISTS Arrendador(
-    ID VARCHAR(10) PRIMARY KEY,
-    FOREIGN KEY (ID) REFERENCES Usuario(ID)
+CREATE TABLE IF NOT EXISTS Medicamento(
+    id VARCHAR(10) PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    descripcion VARCHAR(50) NOT NULL,
+    instrucciones VARCHAR(50) NOT NULL,
+    nombreEmpresa VARCHAR(50) NOT NULL,
+    generico BOOLEAN NOT NULL,
+    fechaVencimiento DATE NOT NULL,
+    precio FLOAT NOT NULL,
+    fechaFabricacion DATE NOT NULL
 );
-
-CREATE TABLE IF NOT EXISTS MensajeIndividual(
-    ID VARCHAR(10) PRIMARY KEY,
-    cuerpo VARCHAR(50) NOT NULL,
+CREATE TABLE IF NOT EXISTS Receta(
+    id VARCHAR(10) PRIMARY KEY,
     fecha DATE NOT NULL,
-    estado BOOLEAN NOT NULL
+    dniMedico VARCHAR(8) NOT NULL,
+    FOREIGN KEY (dniMedico) REFERENCES Medico(dni),
+    dniPaciente VARCHAR(8) NOT NULL,
+    FOREIGN KEY (dniPaciente) REFERENCES Paciente(dni),
+    idConsultorio VARCHAR(10) NOT NULL,
+    FOREIGN KEY (idConsultorio) REFERENCES Consultorio(id)
 );
-
-CREATE TABLE IF NOT EXISTS ChatIndividual(
-    ID VARCHAR(10) PRIMARY KEY,
-    fechacreacion DATE NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS Comentarios(
-    ID VARCHAR(10) PRIMARY KEY,
-    cuerpo VARCHAR(50) NOT NULL,
+CREATE TABLE IF NOT EXISTS Cita(
+    id VARCHAR(10) PRIMARY KEY,
     fecha DATE NOT NULL,
-    estado BOOLEAN NOT NULL,
-    cantidadReacciones INTEGER NOT NULL
+    hora TIME NOT NULL,
+    idConsultorio VARCHAR(10) NOT NULL,
+    FOREIGN KEY (idConsultorio) REFERENCES Consultorio(id),
+    dniMedico VARCHAR(8) NOT NULL,
+    FOREIGN KEY (dniMedico) REFERENCES Medico(dni),
+    dniPaciente VARCHAR(8) NOT NULL,
+    FOREIGN KEY (dniPaciente) REFERENCES Paciente(dni)
 );
-
-CREATE TABLE IF NOT EXISTS Resena(
-    ID VARCHAR(10) PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS Diagnostico(
+    id VARCHAR(10) PRIMARY KEY,
     fecha DATE NOT NULL,
-    valoracion INTEGER NOT NULL,
-    cuerpo VARCHAR(50) NOT NULL
+    dniMedico VARCHAR(8) NOT NULL,
+    FOREIGN KEY (dniMedico) REFERENCES Medico(dni),
+    dniPaciente VARCHAR(8) NOT NULL,
+    FOREIGN KEY (dniPaciente) REFERENCES Paciente(dni),
+    idCita VARCHAR(10) NOT NULL,
+    FOREIGN KEY (idCita) REFERENCES Cita(id),
+    idConsultorio VARCHAR(10) NOT NULL,
+    FOREIGN KEY (idConsultorio) REFERENCES Consultorio(id)
 );
-
-CREATE TABLE IF NOT EXISTS PublicacionInicio(
-    ID VARCHAR(10) PRIMARY KEY,
-    contenido VARCHAR(50) NOT NULL,
-    compartidos INTEGER NOT NULL,
-    meGustas INTEGER NOT NULL,
-    estado BOOLEAN NOT NULL,
-    fecha DATE NOT NULL,
-    cantidadComentarios INTEGER NOT NULL
+CREATE TABLE IF NOT EXISTS Sufre(
+    idEnfermedad VARCHAR(10) NOT NULL,
+    FOREIGN KEY (idEnfermedad) REFERENCES Enfermedad(id),
+    idDiagnostico VARCHAR(10) NOT NULL,
+    FOREIGN KEY (idDiagnostico) REFERENCES Diagnostico(id)
 );
-
-CREATE TABLE IF NOT EXISTS PublicacionAlojamiento(
-    ID VARCHAR(10) PRIMARY KEY ,
-    estado BOOLEAN NOT NULL,
-    fecha DATE NOT NULL,
-    disponibilidad BOOLEAN NOT NULL,
-    promediorating INTEGER NOT NULL,
-    cantidadrese˜nas INTEGER NOT NULL
+CREATE TABLE IF NOT Contiene(
+    idReceta VARCHAR(10) PRIMARY KEY NOT NULL,
+    FOREIGN KEY (idReceta) REFERENCES Receta(id),
+    idMedicamento VARCHAR(10) PRIMARY KEY NOT NULL,
+    FOREIGN KEY (idMedicamento) REFERENCES Medicamento(id),
+    cantidad INTEGER NOT NULL
 );
-
-CREATE TABLE IF NOT EXISTS Alojamiento(
-    ID VARCHAR(10) PRIMARY KEY ,
-    PublicacionAlojamientoID VARCHAR(10) NOT NULL,
-    ArrendadorID INTEGER NOT NULL,
-    FOREIGN KEY (PublicacionAlojamientoID) REFERENCES PublicacionAlojamiento(ID),
-    FOREIGN KEY (ArrendadorID) REFERENCES Arrendador(ID)
+CREATE TABLE IF NOT EXISTS Diagnosticado(
+    idDiagnostico VARCHAR(10) PRIMARY KEY NOT NULL,
+    FOREIGN KEY (idDiagnostico) REFERENCES Diagnostico(id),
+    idEnfermedad VARCHAR(10) PRIMARY KEY NOT NULL,
+    FOREIGN KEY (idEnfermedad) REFERENCES Enfermedad(id)
 );
-
-CREATE TABLE IF NOT EXISTS Publica1(
-    PublicacionInicioID INTEGER PRIMARY KEY NOT NULL ,
-    UsuarioID INTEGER NOT NULL,
-    FOREIGN KEY (PublicacionInicioID) REFERENCES PublicacionInicio(ID),
-    FOREIGN KEY (UsuarioID) REFERENCES Usuario(ID)
-);
-
-CREATE TABLE IF NOT EXISTS Publica2(
-    PublicacionAlojamientoID INTEGER NOT NULL,
-    UsuarioID INTEGER NOT NULL,
-    FOREIGN KEY (PublicacionAlojamientoID) REFERENCES PublicacionAlojamiento(ID),
-    FOREIGN KEY (UsuarioID) REFERENCES Usuario(ID)
-);
-
-CREATE TABLE IF NOT EXISTS EsAmigo(
-    UsuarioID1 INTEGER NOT NULL PRIMARY KEY ,
-    UsuarioID2 INTEGER NOT NULL PRIMARY KEY ,
-    FOREIGN KEY (UsuarioID1) REFERENCES Usuario(ID),
-    FOREIGN KEY (UsuarioID2) REFERENCES Usuario(ID)
-);
-
-CREATE TABLE IF NOT EXISTS Responde(
-    ComentarioID INTEGER NOT NUll,
-    ComentarioRespuestaID INTEGER NOT NULL PRIMARY KEY ,
-    FOREIGN KEY (ComentarioID) REFERENCES Comentarios(ID),
-    FOREIGN KEY (ComentarioRespuestaID) REFERENCES Comentarios(ID)
-);
-
-CREATE TABLE IF NOT EXISTS ContieneComentario(
-    PublicacionInicioID INTEGER NOT NULL,
-    ComentarioID INTEGER NOT NULL,
-    FOREIGN KEY (PublicacionInicioID) REFERENCES PublicacionInicio(ID),
-    FOREIGN KEY (ComentarioID) REFERENCES Comentarios(ID)
-);
-
-CREATE TABLE IF NOT EXISTS Asociada(
-    PublicacionAlojamientoID INTEGER NOT NULL,
-    AlojamientoID INTEGER NOT NULL,
-    FOREIGN KEY (PublicacionAlojamientoID) REFERENCES PublicacionAlojamiento(ID),
-    FOREIGN KEY (AlojamientoID) REFERENCES Alojamiento(ID)
-);
-
-CREATE TABLE IF NOT EXISTS EscribeResena(
-    UsuarioID INTEGER NOT NULL,
-    ResenaID INTEGER NOT NULL,
-    FOREIGN KEY (UsuarioID) REFERENCES Usuario(ID),
-    FOREIGN KEY (ResenaID) REFERENCES Resena(ID)
-);
-
-CREATE TABLE IF NOT EXISTS DuenoDe(
-    AlojamientoID INTEGER NOT NULL,
-    UsuarioID INTEGER NOT NULL,
-    FOREIGN KEY (AlojamientoID) REFERENCES Alojamiento(ID),
-    FOREIGN KEY (UsuarioID) REFERENCES Usuario(ID)
-);
-
-CREATE TABLE IF NOT EXISTS ContieneComentario2(
-    ComentarioID INTEGER NOT NULL,
-    PublicacionInicioID INTEGER NOT NULL,
-    FOREIGN KEY (ComentarioID) REFERENCES Comentarios(ID),
-    FOREIGN KEY (PublicacionInicioID) REFERENCES PublicacionInicio(ID)
-);
-
-CREATE TABLE IF NOT EXISTS EscribeIndividual(
-    MensajeIndividualID INTEGER NOT NULL,
-    UsuarioID INTEGER NOT NULL,
-    FOREIGN KEY (MensajeIndividualID) REFERENCES MensajeIndividual(ID),
-    FOREIGN KEY (UsuarioID) REFERENCES Usuario(ID)
-);
-
-CREATE TABLE IF NOT EXISTS ContieneMI(
-    MensajeIndividualID INTEGER NOT NULL,
-    ChatIndividualID INTEGER NOT NULL,
-    FOREIGN KEY (MensajeIndividualID) REFERENCES MensajeIndividual(ID),
-    FOREIGN KEY (ChatIndividualID) REFERENCES ChatIndividual(ID)
-);
-
-CREATE TABLE IF NOT EXISTS ParticipaIndividual(
-    ChatIndividualID INTEGER NOT NULL,
-    Usuario1ID INTEGER NOT NULL,
-    Usuario2ID INTEGER NOT NULL,
-    FOREIGN KEY (ChatIndividualID) REFERENCES ChatIndividual(ID),
-    FOREIGN KEY (Usuario1ID) REFERENCES Usuario(ID),
-    FOREIGN KEY (Usuario2ID) REFERENCES Usuario(ID)
-);
-
-CREATE TABLE IF NOT EXISTS Comenta(
-    ComentarioID INTEGER NOT NULL,
-    UsuarioID INTEGER NOT NULL,
-    FOREIGN KEY (ComentarioID) REFERENCES Comentarios(ID),
-    FOREIGN KEY (UsuarioID) REFERENCES Usuario(ID)
-);
+--No escribas mas gpt
 
