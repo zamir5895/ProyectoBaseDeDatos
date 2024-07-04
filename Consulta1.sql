2ea70ff08dbe536192853f50da7c1d42dd6b6991
zamir5895 <lmr293141@gmail.com>
2024-07-04 15:32:23 -0500

    arregle la consulta 1

--- Consulta1.sql
+++ Consulta1.sql
@@ -1,8 +1,11 @@
 SELECT
-    tipo_medicamento,
+    CASE
+        WHEN tipo_medicamento = true THEN 'Generico'
+        ELSE 'No Generico'
+        END AS tipo_medicamento,
     (SUM(total_consumo) * 100.0 / (SELECT SUM(total_consumo)
                                    FROM (
-                                            SELECT m.generico AS tipo_medicamento, COUNT(*) AS total_consumo
+                                            SELECT medicamentos.generico AS tipo_medicamento, COUNT(*) AS total_consumo
                                             FROM (
                                                      SELECT c.dniPaciente
                                                      FROM Cita c
@@ -25,7 +28,7 @@ SELECT
                                             GROUP BY medicamentos.generico
                                         ) AS subquery)) AS porcentaje_consumo
 FROM (
-         SELECT m.generico AS tipo_medicamento, COUNT(*) AS total_consumo
+         SELECT medicamentos.generico AS tipo_medicamento, COUNT(*) AS total_consumo
          FROM (
                   SELECT c.dniPaciente
                   FROM Cita c
@@ -48,5 +51,7 @@ FROM (
          GROUP BY medicamentos.generico
      ) AS medicamentos_consumo
 GROUP BY tipo_medicamento;
+
 --"¿Cuál es el porcentaje de consumo de medicamentos genéricos
 -- versus no genéricos por pacientes que han tenido más de 5 citas por año en los últimos 10 años?"
+
